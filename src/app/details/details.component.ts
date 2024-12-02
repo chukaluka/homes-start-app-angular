c6f06ced026ca90cbbe3199ce22ee4bd7d0dc86d
Chukwuka Okonkwo David <chukwukaokonkwo99@gmail.com>
2024-12-02 08:47:00 +0100

    first commit

---
+++ src/app/details/details.component.ts
@@ -0,0 +1,19 @@
+import { HousingLocation } from '../housing-location';
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-details',
+  imports: [CommonModule],
+  templateUrl: './details.component.html',
+  styleUrl: './details.component.css'
+})
+export class DetailsComponent {
+  route: ActivatedRoute = inject(ActivatedRoute);
+  housingLocationId = 0;
+
+  constructor() {
+    this.housingLocationId = Number(this.route.snapshot.params['id']);
+  }
+}
